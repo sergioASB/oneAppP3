eb542217b80685535944b886131d2f423513d11d
Sergio <sergio.salinas.b@ucb.edu.bo>
2022-10-16 16:04:13 -0400

    Initial commit

---
+++ app/src/main/java/edu/bo/ucb/oneapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package edu.bo.ucb.oneapp
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+    val btnRed: Button
+        get() = findViewById(R.id.btn_red)
+    val btnWhite: Button
+        get() = findViewById(R.id.btn_white)
+    val layoutPrincipal: ConstraintLayout
+        get() = findViewById(R.id.layoutPrincipal)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnRed.setOnClickListener {
+            layoutPrincipal.setBackgroundColor(Color.RED)
+        }
+
+        btnWhite.setOnClickListener {
+            layoutPrincipal.setBackgroundColor(Color.WHITE)
+        }
+
+    }
+}
